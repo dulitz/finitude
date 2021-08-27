4b3f8032f8870fe25b0ed324d3a410511fefdc09
dulitz <dulitz@gmail.com>
2021-08-26 21:53:46 -0700

    add standalone command-line send-a-frame, get-an-ack feature

--- frames.py
+++ frames.py
@@ -391,17 +391,81 @@ class CRC16:
     return crc
 
 
+class FrameToSend:
+  def __init__(self, bus, source, dest, funcstr, register='', mask='', data=''):
+    self.bus = bus
+    for f in Function:
+      if f.name == funcstr:
+        func = f.value
+        break
+    else:
+      raise Exception(f'unknown function {funcstr}')
+    regb = (bytes([0]) + FrameToSend.convert_word_to_bytes(register)) if register else b''
+    maskb = (bytes([0]) + FrameToSend.convert_word_to_bytes(mask)) if mask else b''
+    datab = bytes([int(hi + lo, 16) for (hi, lo) in zip(*([iter(data)]*2))])
+    self.frame = AssembledFrame(FrameToSend.convert_word_to_bytes(dest),
+                                FrameToSend.convert_word_to_bytes(source),
+                                func,
+                                regb + maskb + datab)
+    self.sent = False
+
+  def process(self, frame):
+    """Send frame if we haven't sent it yet and if we can. If a sent frame
+    has been acknowledged, return True; if not return False.
+    """
+    if frame.func == Function.ACK06 and not self.sent:
+      self.sent = self.bus.write(self.frame.framebytes)
+      if not self.sent:
+        print('failed to send frame, retrying...', file=sys.stderr)
+    if self.sent and frame.source == self.frame.dest an frame.dest == self.frame.source and frame.func in (Function.ACK06, Function.ACK02, Function.NACK):
+      print(f'transaction complete with {frame}', file=sys.stderr)
+      return True
+    return False
+
+  @staticmethod
+  def convert_word_to_bytes(word):
+    if len(addr) != 4:
+      raise CarrierError(f'{addr} is invalid')
+    assert int(addr, 16)  # raises ValueError if not valid hex
+    return bytes([0, int(addr[0:2], 16], int(addr[2:], 16)])
+
 def main(args):
   """
   All args except the first are ignored. The first is a special file or URI of
   the RS-485 bus adapter. We write nothing to the bus and simply print one line
   for each frame we see on the bus.
   """
+  if len(args) != 2 and (len(args) < 5 or len(args) > 8):
+    print(f'''Usage: {args[0]} URI_of_bus_adapter dest source func data
+Usage: {args[0]} URI_of_bus_adapter dest source READ register
+Usage: {args[0]} URI_of_bus_adapter dest source WRITE register [mask] data''', file=sys.stderr)
+    return 1
   stream = StreamFactory(args[1])
   bus = Bus(stream, report_crc_error=lambda: print('.', end='', file=sys.stderr))
+  if len(args) >= 5:
+    (dest, source, func, data) = (args[2], args[3], args[4], args[5])
+    register = ''
+    mask = ''
+    if func == 'READ':
+      assert len(args) == 6, args
+      register = data
+      data = ''
+    elif func == 'WRITE':
+      assert len(args) == 8, args
+      register = data
+      mask = args[6]
+      data = args[7]
+    else:
+      assert len(args) == 5, args
+    pending = FrameToSend(bus, dest, source, func, register, mask, data)
+  else:
+    pending = None
+
   while True:
     frame = ParsedFrame(bus.read())
     print(frame)
+    if pending and pending.process(frame):
+      pending = None
 
 if __name__ == '__main__':
     import sys
