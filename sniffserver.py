e1a0fc98a21ab11cf5469cad1674234a399157b8
dulitz <dulitz@gmail.com>
2021-08-26 22:35:36 -0700

    fix identifier error

--- sniffserver.py
+++ sniffserver.py
@@ -28,10 +28,10 @@ class _ThreadingWSGISniffServer(ThreadingMixIn, WSGIServer):
 
 
 def convert_word_to_bytes(word):
-    if len(addr) != 4:
-        raise frames.CarrierError(f'{addr} is invalid')
-    assert int(addr, 16)  # raises ValueError if not valid hex
-    return bytes([0, int(addr[0:2], 16), int(addr[2:], 16)])
+    if len(word) != 4:
+        raise frames.CarrierError(f'{word} is invalid')
+    assert int(word, 16)  # raises ValueError if not valid hex
+    return bytes([0, int(word[0:2], 16), int(word[2:], 16)])
 
 
 def start_sniffserver(port, monitors):
